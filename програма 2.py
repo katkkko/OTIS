fd95a408bd2351ad5adc10a5fe55b3e382e218cb
Катерина_Сегеда <113420708+katkkko@users.noreply.github.com>
2022-09-17 15:25:09 +0300

    Add files via upload

---
+++ програма 2.py
@@ -0,0 +1,9 @@
+a = float(input("A трикутника: "))
+b = float(input("B трикутника: "))
+c = float(input("C трикутника: "))
+a1 = a/2
+b1 = b/2
+c1 = c/2
+print(f"A сер. = {a1}")
+print(f"B сер. = {b1}")
+print(f"C сер. = {c1}")
---
+++ програма.py
@@ -0,0 +1,5 @@
+a=input("Число:")
+print(f"Добуток = {int(a[0])*int(a[-1])}")
+print(f"Сума = {int(a[1])+int(a[2])}")
+
+
